52105e72b414a720f275eeefdfbf4115c880c810
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-03-06 19:13:46 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', currentTime => {
+  localStorage.setItem('VIDEOPLAYER-CURRENT-TIME', JSON.stringify(currentTime));
+});
+
+const time = [localStorage.getItem('VIDEOPLAYER-CURRENT-TIME')];
+console.log(time);
