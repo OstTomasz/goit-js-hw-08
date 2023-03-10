73322a8a56014189027fcf554ed0a3fd9a624c3e
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-03-10 20:29:19 +0100

    gh

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+// import throttle from 'lodash.throttle';
 
+const email = document.querySelector('form.feedback-form input[name="email"]');
+const message = document.querySelector(
+  'form.feedback-form textarea[name="message"]'
+);
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', () => {
+  localStorage.setItem('feedback-form-state', [email.value, message.value]);
+});
+
+const savedEmail = localStorage.getItem('feedback-form-state'[0]);
+console.log(savedEmail);
