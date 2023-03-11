494d5bd1156eacaf5d21ec1c96b9e0bef86856ca
OstTomasz <114577269+OstTomasz@users.noreply.github.com>
2023-03-11 14:53:00 +0000

    Deploying to gh-pages from @ OstTomasz/goit-js-hw-08@e9f1a25f8ba095c705808deabc17dc9e1fc14bd9 🚀

--- 03-feedback.45a66ed2.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('form.feedback-form input[name="email"]'),t=document.querySelector('form.feedback-form textarea[name="message"]');document.querySelector(".feedback-form").addEventListener("input",(()=>{localStorage.setItem("feedback-form-state",[e.value,t.value])}));const o=localStorage.getItem("f");console.log(o);
-//# sourceMappingURL=03-feedback.45a66ed2.js.map
--- 03-feedback.d8275490.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector('form.feedback-form input[name="email"]'),t=document.querySelector('form.feedback-form textarea[name="message"]');document.querySelector(".feedback-form").addEventListener("input",(function(){localStorage.setItem("feedback-form-state",[e.value,t.value])}));var o=localStorage.getItem("f");console.log(o)}();
-//# sourceMappingURL=03-feedback.d8275490.js.map
